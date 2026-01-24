cc54f27a947433b9a38012101166ac05ac692f7a
Marek Dohnal <datgaspkitteh@gmail.com>
2026-01-24 16:29:00 +0100

    Good dropdown + aligned stacked bar chart

--- src/app.py
+++ src/app.py
@@ -2,27 +2,12 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
-from layouts import create_layout
-from figures.radar import create_radar
-from figures.choropleth import create_choropleth
-from figures.butterfly import create_butterfly
-
-## Sample data
-#df = pd.DataFrame({
-#    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-#    "Amount": [4, 1, 2, 2, 4, 5],
-#    "City": ["SF", "SF", "SF", "NYC", "NYC", "NYC"]
-#})
-#
-## Create Dash app
-#app = Dash(__name__)
-#app.layout = html.Div([
-#    html.H1("Minimal Dash App"),
-#    dcc.Graph(
-#        id='example-graph',
-#        figure=px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-#    )
-#])
+from .layouts import create_layout, METRIC_OPTIONS
+from .preprocessing import clean_and_convert_types, get_choropleth_data
+from .figures.choropleth import create_choropleth
+from .figures.radar import create_radar_chart
+from .figures.stacked_bar import create_stacked_bar_chart
+from .figures.butterfly import create_butterfly_chart
 
 app = Dash(
     __name__,
@@ -32,6 +17,10 @@ app = Dash(
 #df = pd.read_csv("data/processed/mental_health_clean.csv")
 
 figures = {
+    'choropleth': create_choropleth(choropleth_df, 'Treatment Rate'),
+    'radar': create_radar_chart(),
+    'stacked_bar': create_stacked_bar_chart(),
+    'butterfly': create_butterfly_chart()
 }
 
 app.layout = create_layout()
@@ -40,4 +29,4 @@ app.layout = create_layout()
 server = app.server
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
